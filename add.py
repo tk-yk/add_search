48027457b4501699c4bab2df9751a0d932ec3f65
tk-yk <tkmktuuhan@gmail.com>
2020-05-05 18:15:45 +0900

    refactor-地名検索処理を関数化した

--- add.py
+++ add.py
@@ -1,9 +1,7 @@
 import requests
 
 
-def main():
-    zipcode = "0287111"
-
+def search_address(zipcode):
     response = requests.get(f"https://zip-cloud.appspot.com/api/search?zipcode={zipcode}")
 
     results = response.json()['results']
@@ -13,14 +11,16 @@ def main():
     町域名 = results[0]['address3']
 
     address = f"{都道府県名}{市区町村名}{町域名}"
+    return address
+
+
+def main():
+    zipcode = "0287111"
+
+    address = search_address(zipcode)
     print(address)
 
 
 # zipcode = input("郵便番号は（ハイフンなし番号）>")
 if __name__ == '__main__':
- main()
-
-
-
-
-
+    main()
