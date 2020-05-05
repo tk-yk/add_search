1a315bd41bc8f0a730f49df361d6c3c93e6d767d
tk-yk <tkmktuuhan@gmail.com>
2020-05-05 15:38:37 +0900

    処理をmain関数に移譲」

--- add.py
+++ add.py
@@ -1,18 +1,24 @@
 import requests
+
+
+def main():
+    zipcode = "0287111"
+
+    response = requests.get(f"https://zip-cloud.appspot.com/api/search?zipcode={zipcode}")
+
+    results = response.json()['results']
+
+    都道府県名 = results[0]['address1']
+    市区町村名 = results[0]['address2']
+    町域名 = results[0]['address3']
+
+    address = f"{都道府県名}{市区町村名}{町域名}"
+    print(address)
+
+
 # zipcode = input("郵便番号は（ハイフンなし番号）>")
-zipcode="0287111"
-response = requests.get(f"https://zip-cloud.appspot.com/api/search?zipcode={zipcode}")
-results=response.json()['results']
-都道府県名=results[0]['address1']
-市区町村名=results[0]['address2']
-町域名=results[0]['address3']
-address = f"{都道府県名}{市区町村名}{町域名}"
-
-
-address = search_address(zipcode="028711")
-print(address)
-
-print(address)
+if __name__ == '__main__':
+ main()
 
 
 
--- python02.py
+++ python02.py
@@ -7,6 +7,6 @@ def main():
 
 
 if __name__ == '__main__':
-     main()
+    main()
 
 
